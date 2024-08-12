b8a63b67960c9a566905d15049bb2f3b87ea1809
CreamCrusher <icecreamcrusher21@gmail.com>
2024-08-13 01:18:47 +0300

    update email service

--- services/mailing_service/src/handler.py
+++ services/mailing_service/src/handler.py
@@ -1,17 +1,26 @@
+import json
+
 from src.services.email_service.SMTP import smtp
-from utils.broker.MQBroker import MQBroker
 
 
 class HandlerMQBroker:
 
     @staticmethod
-    async def handler_email_queue(data: dict):
-        email = data.get('email')
-        token = data.get('token')
+    async def handler_email_queue(data: str):
+        try:
+            data = json.loads(data)
+            action = data.get('action')
 
-        test = await smtp.activate_two_factor_authentication(email=email, token=token)
-        print(test)
-        # "Login2FA"Activate2FA
+            match action:
+                case "Activate2FA":
+                    await smtp.activate_two_factor_authentication(data=data, action=action)
+                case "Login2FA":
+                    await smtp.send_login_code_to_email(data, action)
+                case _:
+                    raise "Action not supported"
+
+        except Exception as e:
+            print(e)  # todo залогировать ошибки
 
 
 Handlerbroker: HandlerMQBroker = HandlerMQBroker()
--- services/mailing_service/src/services/email_service/SMTP.py
+++ services/mailing_service/src/services/email_service/SMTP.py
@@ -2,22 +2,24 @@ import httpx
 
 from jinja2 import Template
 
-from utils.base.config import AppSettings
+from utils.base.config import settings
 
 
 class SMTP:
     def __init__(self):
-        super().__init__(base_api="https://api.unisender.com/ru/api")
+        self.base_api = "https://api.unisender.com/ru/api"
         self.session = httpx.AsyncClient()
-        self.key = AppSettings.unisender.token
+        self.key = settings.unisender.token
 
     @staticmethod
     async def init_template(action: str) -> any:
-        path = "services/email_service/template/"
+        path = "src/services/email_service/template/"
 
         match action:
             case "Activate2FA":
                 path += "Activate2FA.html"
+            case "Login2FA":
+                path += "Login2FA.html"
             case _:
                 raise "Action not supported"
 
@@ -34,13 +36,16 @@ class SMTP:
                     email=data.get('email'),
                     token=data.get('token')
                 )
+            case "Login2FA":
+                return template.render(
+                    code=data.get('code'),
+                )
             case _:
                 raise "Action not supported"
 
     async def send(self, data: dict, recipients: str, subject: str, action: str) -> dict:
         url = "https://api.unisender.com/ru/api/sendEmail"
         html_message = await self.get_html_message(action, data)
-
         form_data = {
             'api_key': self.key,
             'email': recipients,
@@ -52,14 +57,27 @@ class SMTP:
         }
         try:
             response = await self.session.post(url, data=form_data)
-            return await response.json()
+            return response.json()
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    async def activate_two_factor_authentication(self, email: str, token: str) -> dict:
-        subject = "Двухфакторная защита успешно подключена"
-        data = {"email": email, "code": token}
-        return await self.send(recipients=email, subject=subject, action='Activate2FA', data=data)
+    async def activate_two_factor_authentication(self, data: dict, action: str) -> dict:
+        subject = "Активация двухфакторной аунтификации"
+        email = data.get('email')
+
+        data = {
+            "token": data.get('jwt_token')
+        }
+        return await self.send(recipients=email, subject=subject, action=action, data=data)
+
+    async def send_login_code_to_email(self, data: dict, action: str) -> dict:
+        subject = "Код подтверждения"
+        email = data.get("email")
+
+        data = {
+            "code": data.get('code'),
+        }
+        return await self.send(recipients=email, subject=subject, action=action, data=data)
 
 
 smtp = SMTP()

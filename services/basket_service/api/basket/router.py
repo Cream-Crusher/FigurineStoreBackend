b98762bac4ce503d3738dc2398638437489e9141
CreamCrusher <icecreamcrusher21@gmail.com>
2024-09-30 22:12:12 +0300

    fix name endpoint and sorted by CRUD

--- services/basket_service/api/basket/router.py
+++ services/basket_service/api/basket/router.py
@@ -6,7 +6,12 @@ from services.basket_service.api.basket.service import basket_service
 router = APIRouter(prefix='/basket', tags=['Baskets|basket'])
 
 
-@router.get('/get_basket_by_user_id', name='Add item for basket', response_model=BasketRead, status_code=201)
+@router.post('/item', name='Add item for basket', status_code=201)
+async def add_item(item: AddItem, baskets=basket_service):
+    return await baskets.add_item(item.user_id, item.item_id, item.quantity)
+
+
+@router.get('/basket/{user_id}', name='Add item for basket', response_model=BasketRead, status_code=201)
 async def add_item(user_id: str, baskets=basket_service):
     basket = await baskets.get(user_id)
 
@@ -15,16 +20,6 @@ async def add_item(user_id: str, baskets=basket_service):
     return basket
 
 
-@router.post('/add_item', name='Add item for basket', status_code=201)
-async def add_item(item: AddItem, baskets=basket_service):
-    return await baskets.add_item(item.user_id, item.item_id, item.quantity)
-
-
-@router.post('/del_item', name='Del item for basket', status_code=201)
-async def del_item(item: DelItem, baskets=basket_service):
-    return await baskets.del_item(item.user_id, item.item_id, item.quantity)
-
-
-@router.post('/del_item', name='Del item for basket', status_code=201)
+@router.delete('/item', name='Del item for basket', status_code=200)
 async def del_item(item: DelItem, baskets=basket_service):
     return await baskets.del_item(item.user_id, item.item_id, item.quantity)
--- services/catalog_service/api/product/router.py
+++ services/catalog_service/api/product/router.py
@@ -8,6 +8,11 @@ from services.catalog_service.api.product.service import product_service
 router = APIRouter(prefix='/product', tags=['Product|Products'])
 
 
+@router.post('/', name='Create Product', status_code=201)
+async def create_product(product: ProductCreate, products=product_service):
+    return await products.create(product.__dict__)
+
+
 @router.get('/', name='Get All Product', response_model=List[ProductRead])
 async def get_all_products(products=product_service):
     return await products.all()
@@ -18,16 +23,11 @@ async def product_by_id(product_id: str, products=product_service):
     return await products.id(product_id)
 
 
-@router.post('/', name='Create Product', status_code=201)
-async def create_product(product: ProductCreate, products=product_service):
-    return await products.create(product.__dict__)
+@router.patch('/', name='Update Product By Id', response_model=ProductRead)
+async def update_product(product_id: str, product: ProductUpdate, products=product_service):
+    return await products.update(product_id, product.__dict__)
 
 
 @router.delete('/{product_id}', name='Delete Product By Id')
 async def del_product(product_id: str, products=product_service):
     return await products.delete(product_id)
-
-
-@router.patch('/', name='Update Product By Id', response_model=ProductRead)
-async def update_product(product_id: str, product: ProductUpdate, products=product_service):
-    return await products.update(product_id, product.__dict__)
